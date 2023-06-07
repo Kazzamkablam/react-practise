aff3e6b2792bfffcfaf930b0dbe24ec3a63a13fe
Kazzamkablam <tottorootorvi@hotmail.com>
2023-06-07 12:21:16 +0300

    Fix for description throwing an error.
    Fixed description throwing an error.

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './Description.css';
 
-function Description({ description, setDescription }) {
+// Expect 'descriptionData' and 'setDescriptionData' props from the parent component
+function Description({ descriptionData, setDescriptionData }) {
+  // Function to handle changes in the textarea
   const handleDescriptionChange = (event) => {
-    setDescription(event.target.value);
+    // Update the description data in the state
+    setDescriptionData(event.target.value);
   };
 
+  // Render the component
   return (
     <div className="description">
       <h2>4. Tell us about yourself</h2>
-      <textarea value={description} onChange={handleDescriptionChange} placeholder="Describe yourself here..." />
+      {/* Bind the textarea to the corresponding state variable and set the onChange handler */}
+      <textarea value={descriptionData} onChange={handleDescriptionChange} placeholder="Describe yourself here..." />
     </div>
   );
 }
 
+// Export the component so it can be imported and used in other files
 export default Description;
